b71242268988c2e95d15da0da4586a54d0034afa
slymodi88 <77759317+slymodi88@users.noreply.github.com>
2021-05-09 00:03:42 +0200

    Delete management/commands directory
    unused file removed

--- management/commands/initialize_database.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import BaseCommand
-
-from user.models import User
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        User.objects.create(name='ahmed', employee_number='1254', password='123456')
